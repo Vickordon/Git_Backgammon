345cd8fa8c46e7966ed0ee91daf6772c8e2f0da8
Vickordon <79508283+Vickordon@users.noreply.github.com>
2021-04-08 09:29:08 +0300

    Add files via upload

---
+++ Coursor and Color.cpp
@@ -0,0 +1,38 @@
+#include<conio.h>
+#include<stdio.h>
+#include<time.h>
+#include<Windows.h>
+#include<string.h>
+#include "gammon.h"
+
+void gotoxy(float x, float y)
+{
+	HANDLE StdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coord = { x, y };
+	SetConsoleCursorPosition(StdOut, coord);
+}
+
+void SetColor(int text, int background)
+{
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hStdOut, (WORD)((background << 4) | text));
+}
+
+void cursorOff()
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO structCursorInfo;
+	GetConsoleCursorInfo(handle,&structCursorInfo);
+	structCursorInfo.bVisible = FALSE;
+	SetConsoleCursorInfo( handle, &structCursorInfo );
+}
+
+void cursorOn()
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO structCursorInfo;
+	GetConsoleCursorInfo(handle,&structCursorInfo);
+	structCursorInfo.bVisible = TRUE;
+	SetConsoleCursorInfo( handle, &structCursorInfo );
+}
+

86434df0725219fa51d2693b530b802c6a74d048
Vickordon <79508283+Vickordon@users.noreply.github.com>
2021-04-08 09:34:17 +0300

    Add files via upload

---
+++ Global.cpp
@@ -0,0 +1,11 @@
+
+
+ int score1, score2, mode, Esc=27, Enter=13, stop=4;
+ int color1 = 2, color2 = 12,  pause = 1000;
+ int x1_global, x2_global, x3_global, x4_global;
+ int x_global, y_global, x_y_global, number, count_step;
+ bool check1, check2, check3, check4, buf1, buf2, buf3, buf4;
+ bool check_x, check_y, buf_x, buf_y, check_home;
+
+
+
